ae4307eca71f9ddc8a2f96f0b1477c50a9609538
Autonvilkku <v.lokasaari13@gmail.com>
2024-06-26 13:34:27 +0300

    initial

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -7,7 +7,7 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "/home/vilkku/coding/cpp/Supernova-Engine/CMakeLists.txt"
+  "/home/vilkku/coding/cpp/SupernovaEngine/CMakeLists.txt"
   "CMakeFiles/3.29.5/CMakeCCompiler.cmake"
   "CMakeFiles/3.29.5/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.29.5/CMakeSystem.cmake"
