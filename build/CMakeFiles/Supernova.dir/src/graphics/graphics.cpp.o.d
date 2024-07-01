8290c788d34171da14827c5fcb04eecca55ea851
Autonvilkku <145136371+Autonvilkku@users.noreply.github.com>
2024-07-01 10:51:58 +0300

    Delete build directory

--- build/CMakeFiles/Supernova.dir/src/graphics/graphics.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Supernova.dir/src/graphics/graphics.cpp.o: \
- /home/vilkku/coding/cpp/SupernovaEngine/src/graphics/graphics.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Supernova.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Supernova.dir/src/main.cpp.o: \
- /home/vilkku/coding/cpp/SupernovaEngine/src/main.cpp \
- /usr/include/stdc-predef.h
